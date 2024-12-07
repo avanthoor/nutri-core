73766fa4820abfdaac6f3ce7f04df0d4f2672ffa
Ann <socevilaf@gmail.com>
2024-12-07 10:23:22 +0300

    add-recipe

--- src/main.tsx
+++ src/main.tsx
@@ -18,12 +18,52 @@ import { ChakraProvider, createSystem, defineConfig } from "@chakra-ui/react";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     {/*TODO: настройка темы
-     в документаци тема передантся именно в ChakraProviderhttps://www.chakra-ui.com/docs/theming/overview 
-    !!но при установке там рекмендуется именно Provider -https://www.chakra-ui.com/docs/get-started/installation*/}
+     в документаци по темизации  App оборачивается в ChakraProvider https://www.chakra-ui.com/docs/theming/overview 
+    !!но при установке там рекмендуется именно Provider. Сюда тему добавить не могу -https://www.chakra-ui.com/docs/get-started/installation*/ }
     <Provider>
       <App />
     </Provider>
 
-    
+{/* TODO: так можно передать тему, но полетит вся разметка😒 */}
+    {/* <ChakraProvider  value={system}>      
+        <App />      
+    </ChakraProvider> */}
+
   </StrictMode>
 );
+
+// TODO: пробовала так внутри provider.tsx - цвета на применяются
+// import {
+//   ChakraProvider,
+//   createSystem,
+//   defineConfig,
+// } from "@chakra-ui/react";
+// import {
+//   ColorModeProvider,
+//   type ColorModeProviderProps,
+// } from "../components/ui/color-mode";
+
+// const config = defineConfig({
+//   theme: {
+//     tokens: {
+//       colors: {
+//         primary: { value: "#0FEE0F" },
+//         secondary: { value: "#EE0F0F" },
+//       },
+//     },
+//   },
+// });
+
+// const system = createSystem(config);
+
+// export function Provider(props: ColorModeProviderProps) {
+//   return (
+//     <ChakraProvider value={system}>
+//           <ColorModeProvider {...props} />
+//     </ChakraProvider>
+//   );
+// }
+
+{/* <Box color="primary" >
+  Hello World
+</Box> */}
